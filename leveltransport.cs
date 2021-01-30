725429dfd64ee779c3da9830dd16922b2e5ea2e9
Cole-Pz <72885218+Cole-Pz@users.noreply.github.com>
2021-01-31 01:26:58 +0500

    Add files via upload

---
+++ leveltransport.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class leveltransport : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void OnTriggerEnter2D(Collider2D other)
+    {
+        if (other.CompareTag("Player"))
+        { //loading level with build index
+            SceneManager.LoadScene(1);
+
+            //Loading level with scene name
+            //SceneManager.LoadScene("SceneName");
+            
+            //Restart Level
+            //SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex);
+
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
